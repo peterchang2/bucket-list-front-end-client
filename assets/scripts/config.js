fa1b8fb5e8982c826824a1e4786d2a278a99e185
kwesiatherley <kwesiatherley@gmail.com>
2019-01-04 13:23:30 -0500

    Create auth folder
    - create api.js file
    - updated config.js file
    
    Peer Programmed with @lucaspchartier @peterchang2

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: ' https://secure-bastion-12965.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
